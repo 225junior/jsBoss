3b6bfe1c445caee1b4ba39aabf4e2dae6752282d
225junior <35270982+225junior@users.noreply.github.com>
2021-11-06 22:52:22 +0000

    get index new element and copy array

--- arrays/concat.js
+++ arrays/concat.js
@@ -1,7 +1,18 @@
-a  = [1,2,3,4]
+a = [1,2,3,4]
 b = [5,6,7,8,9]
-
+c = [10,11,12,13]
 console.log(a.concat(b))
-
-// sortie
 // [1, 2, 3, 4, 5,6, 7, 8, 9]
+
+
+console.log(a.concat(b, c))
+// [1, 2, 3,  4,  5,  6, 7, 8, 9, 10, 11, 12, 13]
+
+// concat permet de creer une copie de tableau
+tabCopy  = c.concat();
+
+// index contien la position du nouvel element a ajouter mais le start est 1
+index = tabCopy.push(14)
+
+console.log(tabCopy)
+
