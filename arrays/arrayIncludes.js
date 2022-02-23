7ac48d8e8119363a1ec313a91e0d50bd8fa9e770
225junior <35270982+225junior@users.noreply.github.com>
2022-02-23 17:49:44 +0000

    ajout de commentaire

--- arrays/arrayIncludes.js
+++ arrays/arrayIncludes.js
@@ -11,7 +11,7 @@ const existant = [
 let nouveau = ["Albert", "lossen", "Monpieux", "leblanc", "tontonmath"];
 
 let tabName = [];
-let Fnouveau = [];
+let tfind = [];
 
 for (let index = 0; index < existant.length; index++) {
     tabName.push(existant[index].split(":")[0]);
@@ -20,17 +20,17 @@ for (let index = 0; index < existant.length; index++) {
 for (let index = 0; index < nouveau.length; index++) {
     for (let key = 0; key < tabName.length; key++) {
         if (tabName[key] == nouveau[index]) {
-            Fnouveau.push(nouveau[index]);
+            tfind.push(nouveau[index]);
         }
     }
 }
 
-console.log(nouveau);
-console.log(tabName);
-console.log(Fnouveau);
+console.log('Nouvau',nouveau);
+// console.log(tabName);
+console.log('Find',tfind);
 
 let difference = nouveau
-    .filter((x) => !Fnouveau.includes(x))
-    .concat(Fnouveau.filter((x) => !Fnouveau.includes(x)));
+    .filter((x) => !tfind.includes(x));
+    // .concat(tfind.filter((x) => !tfind.includes(x)));
 
-console.log(difference);
+console.log('Diference entre nouveau et tFind',difference);
